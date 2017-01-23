e3c3e18486be6ae1a74a9ec37fd5cb33228a2be1
g <a@b.com>
2017-01-23 21:54:17 +0100

    Fixed register-policies.sh to use -address argument

--- register-policies.sh
+++ register-policies.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VAULT_CMD="docker-compose exec vault vault -address=$VAULT_ADDR"
+VAULT_ADDR="http://localhost:8200"
 
 # register all the policies using the Vault CLI tool
 # from within the container
 for x in $(ls "$CURRENT_DIR/vault/policies/")
 do
-  docker-compose exec vault vault policy-write $(echo $x|sed -e 's/\.hcl//') /policies/$x
+  $VAULT_CMD policy-write $(echo $x|sed -e 's/\.hcl//') /policies/$x
 done
 
 # setup a role for the token vendor to use
-docker-compose exec vault vault write auth/token/roles/application allowed_policies=application
+$VAULT_CMD write auth/token/roles/application allowed_policies=application
 
 # put some stuff in the store for the application to read
-docker-compose exec vault vault write secret/my-test-app/secrets test1="I am a secret" test2="I am also a secret"
+$VAULT_CMD write secret/my-test-app/secrets test1="I am a secret" test2="I am also a secret"
 
 echo "Pass this token to the token-vendor:"
-docker-compose exec vault vault token-create -policy="token-vendor" |head -n 3 |tail -n 1
+$VAULT_CMD token-create -policy="token-vendor" |head -n 3 |tail -n 1
